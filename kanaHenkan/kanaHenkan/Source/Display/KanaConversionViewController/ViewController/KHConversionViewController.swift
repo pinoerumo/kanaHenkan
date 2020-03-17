8d63a13a85cb4df89effece5cc11aa597ef3354a
boko <40710040+pinoerumo@users.noreply.github.com>
2020-03-18 00:09:40 +0900

    変換用画面作成 KHConversionView ・キーボードの改行ボタンを「完了」に変更
    KHConversionViewController
    ・TextFieldDelegateを実装
    ・完了ボタン押下時キーボードを下げるように
    ・変換用TextFieldに文字を打った際警告ラベルをHiddenするように

--- kanaHenkan/kanaHenkan/Source/Display/KanaConversionViewController/ViewController/KHConversionViewController.swift
+++ kanaHenkan/kanaHenkan/Source/Display/KanaConversionViewController/ViewController/KHConversionViewController.swift
@@ -19,12 +19,28 @@ class KHConversionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareTextField()
+    }
+    
+    func prepareTextField() {
+        conversionView.conversionTextField.delegate = self
     }
 }
 
 extension KHConversionViewController: KHConversionViewDelegate{
-    
     /// 変換ボタン押下時のデリゲートメソッド
     func conversionView(_ conversionView: KHConversionView, tappedConversionButton button: UIButton) {
+    }
+}
+
+extension KHConversionViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        conversionView.endEditing(true)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        conversionView.cautionLabel.isHidden = true
+        return true
     }
 }
