30d325aca195c250620d722ca4a4b7f104babb19
boko <40710040+pinoerumo@users.noreply.github.com>
2020-03-17 23:13:26 +0900

    変換用画面レイアウト作成 KHConversionView ・UIの配置 ・UIのアウトレット接続

--- kanaHenkan/kanaHenkan/Source/Display/KanaConversionViewController/View/KHConversionView.swift
+++ kanaHenkan/kanaHenkan/Source/Display/KanaConversionViewController/View/KHConversionView.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class KHConversionView: CommonView {
-
+    /**変換文字列が入っていない場合表示するラベル*/
+    @IBOutlet var cautionLabel: UILabel!
+    /**変換文字列入力テキストフィールド*/
+    @IBOutlet var conversionTextField: UITextField!
+    /**変換タイプ選択セグメント*/
+    @IBOutlet var conversionTypeSegment: UISegmentedControl!
+    
+    
+    /// 変換ボタン押下時メソッド
+    /// - Parameter sender: 変換ボタン
+    @IBAction func tappedConversionButton(_ sender: Any) {
+        
+    }
+    
+    
 }
